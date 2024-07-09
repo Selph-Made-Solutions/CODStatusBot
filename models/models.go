1d124a973bb785b5132f87c98179c68172546dc2
bradselph <Bselph512@gmail.com>
2024-07-09 10:14:45 -0400

    Update models.go

--- models/models.go
+++ models/models.go
@@ -6,19 +6,20 @@ import (
 
 type Account struct {
 	gorm.Model
-	GuildID                string `gorm:"index"` // The ID of the guild the account belongs to.
-	UserID                 string `gorm:"index"` // The ID of the user.
-	ChannelID              string // The ID of the channel associated with the account.
-	Title                  string // The title of the account.
-	LastStatus             Status `gorm:"default:unknown"` // The last known status of the account.
-	LastCheck              int64  `gorm:"default:0"`       // The timestamp of the last check performed on the account.
-	LastNotification       int64  // The timestamp of the last daily notification sent out on the account.
-	LastCookieNotification int64  // The timestamp of the last notification sent out on the account for an expired ssocookie.
-	SSOCookie              string // The SSO cookie associated with the account.
-	Created                string // The timestamp of when the account was created on Activision.
-	IsExpiredCookie        bool   `gorm:"default:false"`   // A flag indicating if the SSO cookie has expired.
-	NotificationType       string `gorm:"default:channel"` // User preference for location of notifications either channel or dm
-	IsPermabanned          bool   `gorm:"default:false"`   // A flag indicating if the account is permanently banned
+	GuildID                  string `gorm:"index"` // The ID of the guild the account belongs to.
+	UserID                   string `gorm:"index"` // The ID of the user.
+	ChannelID                string // The ID of the channel associated with the account.
+	Title                    string // The title of the account.
+	LastStatus               Status `gorm:"default:unknown"` // The last known status of the account.
+	LastCheck                int64  `gorm:"default:0"`       // The timestamp of the last check performed on the account.
+	LastNotification         int64  // The timestamp of the last daily notification sent out on the account.
+	LastCookieNotification   int64  // The timestamp of the last notification sent out on the account for an expired ssocookie.
+	SSOCookie                string // The SSO cookie associated with the account.
+	Created                  string // The timestamp of when the account was created on Activision.
+	IsExpiredCookie          bool   `gorm:"default:false"`   // A flag indicating if the SSO cookie has expired.
+	NotificationType         string `gorm:"default:channel"` // User preference for location of notifications either channel or dm
+	IsPermabanned            bool   `gorm:"default:false"`   // A flag indicating if the account is permanently banned
+	LastPermabanNotification int64  // The timestamp of the last notification sent out on the account for a permanent banned account.
 }
 
 type Ban struct {
