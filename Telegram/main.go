372897d81422ce7292e4b5adfff4e49b34c121b1
bradselph <Bselph512@gmail.com>
2024-07-11 04:43:23 -0400

    Create main.go

---
+++ Telegram/main.go
@@ -0,0 +1,16 @@
+package Telegram
+
+require (
+
+	"github.com/mymmrac/telego"
+
+)
+
+commands list
+accountage - Check the age of an account
+accountlogs - View the logs for an account
+addaccount - Add or remove an account for shadowban checking
+checknow - Immediately check the status of a specific account``
+help - Simple guide to getting your SSOCookie
+removeaccount - Remove an account from shadowban checking
+updateaccount - Update the SSO cookie for an account
