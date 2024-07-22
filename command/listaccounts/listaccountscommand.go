b0c6c9bb8e4de8d443f83bb781c1eefc23e526f5
bradselph <Bselph512@gmail.com>
2024-07-22 10:49:05 -0400

    Update listaccountscommand.go

--- command/listaccounts/listaccountscommand.go
+++ command/listaccounts/listaccountscommand.go
@@ -6,6 +6,7 @@ import (
 	"CODStatusBot/models"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"gorm.io/gorm"
 )
 
 func RegisterCommand(s *discordgo.Session, guildID string) {
@@ -39,11 +40,31 @@ func UnregisterCommand(s *discordgo.Session, guildID string) {
 }
 
 func CommandListAccounts(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	userID := i.Member.User.ID
-	guildID := i.GuildID
+	var userID, guildID string
+	// Check if the interaction is from a guild or DM
+	if i.Member != nil {
+		userID = i.Member.User.ID
+		guildID = i.GuildID
+	} else if i.User != nil {
+		userID = i.User.ID
+		// In DMs, we don't have a guildID, so we'll leave it empty
+	} else {
+		logger.Log.Error("Interaction doesn't have Member or User")
+		respondToInteraction(s, i, "An error occurred while processing your request.")
+		return
+	}
 
 	var accounts []models.Account
-	result := database.DB.Where("user_id = ? AND guild_id = ?", userID, guildID).Find(&accounts)
+	var result *gorm.DB
+
+	if guildID != "" {
+		// If we have a guildID, query for accounts in this specific guild
+		result = database.DB.Where("user_id = ? AND guild_id = ?", userID, guildID).Find(&accounts)
+	} else {
+		// If we don't have a guildID (DM context), query for all accounts of this user
+		result = database.DB.Where("user_id = ?", userID).Find(&accounts)
+	}
+
 	if result.Error != nil {
 		logger.Log.WithError(result.Error).Error("Error fetching user accounts")
 		respondToInteraction(s, i, "Error fetching your accounts. Please try again.")
@@ -57,7 +78,7 @@ func CommandListAccounts(s *discordgo.Session, i *discordgo.InteractionCreate) {
 
 	var accountList string
 	for _, account := range accounts {
-		accountList += fmt.Sprintf("• %s (Status: %s)\n", account.Title, account.LastStatus)
+		accountList += fmt.Sprintf("• %s (Status: %s, Guild: %s)\n", account.Title, account.LastStatus, account.GuildID)
 	}
 
 	embed := &discordgo.MessageEmbed{
