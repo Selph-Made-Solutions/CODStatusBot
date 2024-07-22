6f87c9129bba014d8051cd1f61b664d472a31f73
bradselph <Bselph512@gmail.com>
2024-07-22 03:08:56 -0400

    Update header.go

--- services/header.go
+++ services/header.go
@@ -8,6 +8,7 @@ import (
 // It includes the SSO cookie for authentication.
 func GenerateHeaders(ssoCookie string) map[string]string {
 	return map[string]string{
+		"user-agent":     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0",
 		"accept":         "*/*",
 		"sec-fetch-mode": "cors",
 		"cookie":         fmt.Sprintf("ACT_SSO_COOKIE=%s", ssoCookie),
