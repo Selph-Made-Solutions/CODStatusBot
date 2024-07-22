5635d4b7f1e11faf47053f90a1f7125099b4c5c4
bradselph <Bselph512@gmail.com>
2024-07-22 03:11:19 -0400

    Update bot.go

--- bot/bot.go
+++ bot/bot.go
@@ -8,7 +8,7 @@ import (
 	"CODStatusBot/command/removeaccountnew"
 	"CODStatusBot/command/updateaccountnew"
 	"CODStatusBot/logger"
-	"CODStatusBot/services"
+	// "CODStatusBot/services"
 	"errors"
 	"github.com/bwmarrin/discordgo"
 	"os"
@@ -42,22 +42,14 @@ func StartBot() error {
 		return err
 	}
 
-	guilds, err := discord.UserGuilds(100, "", "", false)
-	if err != nil {
-		logger.Log.WithError(err).WithField("Bot startup", "Initiating Guilds").Error()
-		return err
-	}
-	for _, guild := range guilds {
-		logger.Log.WithField("guild", guild.Name).Info("Connected to guild")
-		command.RegisterCommands(discord, guild.ID)
-	}
+	command.RegisterCommands(discord)
+	logger.Log.Info("Registering global commands")
 
 	discord.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		switch i.Type {
 		case discordgo.InteractionApplicationCommand:
 			if h, ok := command.Handlers[i.ApplicationCommandData().Name]; ok {
 				logger.Log.WithField("command", i.ApplicationCommandData().Name).Info("Handling command")
-
 				h(s, i)
 			} else {
 				logger.Log.WithField("command", i.ApplicationCommandData().Name).Error("Command handler not found")
@@ -94,20 +86,7 @@ func StartBot() error {
 			}
 		}
 	})
-	discord.AddHandler(OnGuildCreate)
-	discord.AddHandler(OnGuildDelete)
-	go services.CheckAccounts(discord)
+
+	// go services.CheckAccounts(discord)
 	return nil
-}
-
-func OnGuildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
-	guildID := event.Guild.ID
-	logger.Log.WithField("guild", guildID).Info("Bot joined server:")
-	command.RegisterCommands(s, guildID)
-}
-
-func OnGuildDelete(s *discordgo.Session, event *discordgo.GuildDelete) {
-	guildID := event.Guild.ID
-	logger.Log.WithField("guild", guildID).Info("Bot left guild")
-	command.UnregisterCommands(s, guildID)
 }
