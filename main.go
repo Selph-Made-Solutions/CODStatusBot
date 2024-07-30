1b037e251be394f2c0129b22a58e140f27cfae6f
bradselph <Bselph512@gmail.com>
2024-07-30 03:57:20 -0400

    Update main.go

--- main.go
+++ main.go
@@ -26,12 +26,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	err = services.LoadTwoCaptchaEnvironmentVariables()
-	if err != nil {
-		logger.Log.WithError(err).Fatal("Failed to load 2captcha environment variables")
-		os.Exit(1)
-	}
-
 	err = database.Databaselogin()
 	if err != nil {
 		logger.Log.WithError(err).WithField("Bot Startup", "Database login").Error()
@@ -41,7 +35,6 @@ func main() {
 	err = bot.StartBot() // Start the Discord bot.
 
 	if err != nil {
-
 		logger.Log.WithError(err).WithField("Bot Startup", "Discord login").Error()
 		os.Exit(1)
 	}
@@ -66,8 +59,6 @@ func loadEnvironmentVariables() error {
 		"EZCAPTCHA_CLIENT_KEY",
 		"RECAPTCHA_SITE_KEY",
 		"RECAPTCHA_URL",
-		// "TWOCAPAPPID",
-		"TWOCAPTCHA_API_KEY",
 	}
 
 	for _, envVar := range requiredEnvVars {
