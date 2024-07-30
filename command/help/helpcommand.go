4d25f84c971606b5f659808eeb397ac3d0750c83
bradselph <Bselph512@gmail.com>
2024-07-30 13:14:47 -0400

    Update helpcommand.go

--- command/help/helpcommand.go
+++ command/help/helpcommand.go
@@ -6,66 +6,6 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-func RegisterCommand(s *discordgo.Session, guildID string) {
-	commands := []*discordgo.ApplicationCommand{
-		{
-			Name:        "help",
-			Description: "Simple guide to getting your SSOCookie",
-		},
-	}
-
-	existingCommands, err := s.ApplicationCommands(s.State.User.ID, guildID)
-	if err != nil {
-		logger.Log.WithError(err).Error("Error getting application commands")
-		return
-	}
-
-	var existingCommand *discordgo.ApplicationCommand
-	for _, command := range existingCommands {
-		if command.Name == "help" {
-			existingCommand = command
-			break
-		}
-	}
-
-	newCommand := commands[0]
-
-	if existingCommand != nil {
-		logger.Log.Info("Updating help command")
-		_, err = s.ApplicationCommandEdit(s.State.User.ID, guildID, existingCommand.ID, newCommand)
-		if err != nil {
-			logger.Log.WithError(err).Error("Error updating help command")
-			return
-		}
-	} else {
-		logger.Log.Info("Creating help command")
-		_, err = s.ApplicationCommandCreate(s.State.User.ID, guildID, newCommand)
-		if err != nil {
-			logger.Log.WithError(err).Error("Error creating help command")
-			return
-		}
-	}
-}
-
-func UnregisterCommand(s *discordgo.Session, guildID string) {
-	commands, err := s.ApplicationCommands(s.State.User.ID, guildID)
-	if err != nil {
-		logger.Log.WithError(err).Error("Error getting application commands")
-		return
-	}
-
-	for _, command := range commands {
-		logger.Log.Infof("Deleting command %s", command.Name)
-		err := s.ApplicationCommandDelete(s.State.User.ID, guildID, command.ID)
-		if err != nil {
-
-			logger.Log.WithError(err).Errorf("Error deleting command %s", command.Name)
-
-			return
-		}
-	}
-}
-
 func CommandHelp(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	logger.Log.Info("Received help command")
 	helpGuide := "CODStatusBot Help Guide\n\n" +
