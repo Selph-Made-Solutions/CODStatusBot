981a568e86292e146f2b87f502b6236f0e87579e
bradselph <Bselph512@gmail.com>
2024-08-08 07:56:30 -0400

    Update user_settings.go

--- services/user_settings.go
+++ services/user_settings.go
@@ -8,6 +8,23 @@ import (
 	"strconv"
 )
 
+var defaultSettings models.UserSettings
+
+func init() {
+	checkInterval, _ := strconv.Atoi(os.Getenv("CHECK_INTERVAL"))
+	notificationInterval, _ := strconv.ParseFloat(os.Getenv("NOTIFICATION_INTERVAL"), 64)
+	cooldownDuration, _ := strconv.ParseFloat(os.Getenv("COOLDOWN_DURATION"), 64)
+	statusChangeCooldown, _ := strconv.ParseFloat(os.Getenv("STATUS_CHANGE_COOLDOWN"), 64)
+
+	defaultSettings = models.UserSettings{
+		CheckInterval:        checkInterval,
+		NotificationInterval: notificationInterval,
+		CooldownDuration:     cooldownDuration,
+		StatusChangeCooldown: statusChangeCooldown,
+		NotificationType:     "channel",
+	}
+}
+
 func GetUserSettings(userID string) (models.UserSettings, error) {
 	logger.Log.Infof("Getting user settings for user: %s", userID)
 	var settings models.UserSettings
@@ -17,19 +34,42 @@ func GetUserSettings(userID string) (models.UserSettings, error) {
 		return settings, result.Error
 	}
 
-	// Set default values if not set
-	if settings.CheckInterval == 0 {
-		settings.CheckInterval, _ = strconv.Atoi(os.Getenv("CHECK_INTERVAL"))
+	// If the user doesn't have a captcha key, use default settings
+	if settings.CaptchaAPIKey == "" {
+		settings.CheckInterval = defaultSettings.CheckInterval
+		settings.NotificationInterval = defaultSettings.NotificationInterval
+		settings.CooldownDuration = defaultSettings.CooldownDuration
+		settings.StatusChangeCooldown = defaultSettings.StatusChangeCooldown
+		settings.NotificationType = defaultSettings.NotificationType
 	}
-	if settings.NotificationInterval == 0 {
-		settings.NotificationInterval, _ = strconv.ParseFloat(os.Getenv("NOTIFICATION_INTERVAL"), 64)
-	}
-	if settings.CooldownDuration == 0 {
-		settings.CooldownDuration, _ = strconv.ParseFloat(os.Getenv("COOLDOWN_DURATION"), 64)
-	}
-	if settings.StatusChangeCooldown == 0 {
-		settings.StatusChangeCooldown, _ = strconv.ParseFloat(os.Getenv("STATUS_CHANGE_COOLDOWN"), 64)
-	}
+
 	logger.Log.Infof("Got user settings for user: %s", userID)
 	return settings, nil
+}
+
+func GetDefaultSettings() (models.UserSettings, error) {
+	return defaultSettings, nil
+}
+
+func RemoveCaptchaKey(userID string) error {
+	var settings models.UserSettings
+	result := database.DB.Where(models.UserSettings{UserID: userID}).First(&settings)
+	if result.Error != nil {
+		logger.Log.WithError(result.Error).Error("Error getting user settings")
+		return result.Error
+	}
+
+	settings.CaptchaAPIKey = ""
+	settings.CheckInterval = defaultSettings.CheckInterval
+	settings.NotificationInterval = defaultSettings.NotificationInterval
+	settings.CooldownDuration = defaultSettings.CooldownDuration
+	settings.StatusChangeCooldown = defaultSettings.StatusChangeCooldown
+	settings.NotificationType = defaultSettings.NotificationType
+
+	if err := database.DB.Save(&settings).Error; err != nil {
+		logger.Log.WithError(err).Error("Error saving user settings")
+		return err
+	}
+
+	return nil
 }
