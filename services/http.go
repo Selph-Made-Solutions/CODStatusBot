13405c6ab0754e99cad377820ea4a43e69cdeb94
bradselph <Bselph512@gmail.com>
2024-08-15 06:18:46 -0400

    Update http.go

--- services/http.go
+++ services/http.go
@@ -51,8 +51,14 @@ func VerifySSOCookie(ssoCookie string) bool {
 }
 
 // CheckAccount checks the account status associated with the provided SSO cookie.
-func CheckAccount(ssoCookie string, captchaAPIKey string) (models.Status, error) {
+func CheckAccount(ssoCookie string, userID string) (models.Status, error) {
 	logger.Log.Info("Starting CheckAccount function")
+
+	captchaAPIKey, err := GetUserCaptchaKey(userID)
+	if err != nil {
+		logger.Log.WithError(err).Error("Failed to get user's captcha API key")
+		return models.StatusUnknown, fmt.Errorf("failed to get user's captcha API key: %v", err)
+	}
 
 	gRecaptchaResponse, err := SolveReCaptchaV2WithKey(captchaAPIKey)
 	if err != nil {
