67bb379a198aafdc56520dee6b61a62eb386ca7e
bradselph <Bselph512@gmail.com>
2024-08-15 15:45:38 -0400

    Update models.go

--- models/models.go
+++ models/models.go
@@ -22,6 +22,7 @@ type Account struct {
 	IsPermabanned          bool   `gorm:"default:false"`   // A flag indicating if the account is permanently banned
 	LastCookieCheck        int64  `gorm:"default:0"`       // The timestamp of the last cookie check for permanently banned accounts
 	LastStatusChange       int64  `gorm:"default:0"`       // The timestamp of the last status change
+	IsCheckDisabled        bool   `gorm:"default:false"`   // A flag indicating if checks are disabled for this account
 }
 
 type UserSettings struct {
