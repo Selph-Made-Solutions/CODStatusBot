dab55670a3f1465762d12fa21fc6e4f22edcc2c3
bradselph <Bselph512@gmail.com>
2024-09-01 16:05:56 -0400

    Update models.go

--- models/models.go
+++ models/models.go
@@ -6,16 +6,16 @@ import (
 
 type Account struct {
 	gorm.Model
-	GuildID                string `gorm:"index"` // The ID of the guild the account belongs to.
-	UserID                 string `gorm:"index"` // The ID of the user.
-	ChannelID              string // The ID of the channel associated with the account.
-	Title                  string // The title of the account.
-	LastStatus             Status `gorm:"default:unknown"` // The last known status of the account.
-	LastCheck              int64  `gorm:"default:0"`       // The timestamp of the last check performed on the account.
-	LastNotification       int64  // The timestamp of the last daily notification sent out on the account.
-	LastCookieNotification int64  // The timestamp of the last notification sent out on the account for an expired ssocookie.
-	SSOCookie              string // The SSO cookie associated with the account.
-	SSOCookieExpiration    int64
+	GuildID                string           `gorm:"index"` // The ID of the guild the account belongs to.
+	UserID                 string           `gorm:"index"` // The ID of the user.
+	ChannelID              string           // The ID of the channel associated with the account.
+	Title                  string           // The title of the account.
+	LastStatus             AccountStatus    `gorm:"serializer:json"` // The last known status of the account.
+	LastCheck              int64            `gorm:"default:0"`       // The timestamp of the last check performed on the account.
+	LastNotification       int64            // The timestamp of the last daily notification sent out on the account.
+	LastCookieNotification int64            // The timestamp of the last notification sent out on the account for an expired ssocookie.
+	SSOCookie              string           // The SSO cookie associated with the account.
+	SSOCookieExpiration    int64            // The timestamp of the expiration of the SSO cookie.
 	Created                string           // The timestamp of when the account was created on Activision.
 	IsExpiredCookie        bool             `gorm:"default:false"`   // A flag indicating if the SSO cookie has expired.
 	NotificationType       string           `gorm:"default:channel"` // User preference for location of notifications either channel or dm
@@ -23,19 +23,7 @@ type Account struct {
 	LastCookieCheck        int64            `gorm:"default:0"`       // The timestamp of the last cookie check for permanently banned accounts
 	LastStatusChange       int64            `gorm:"default:0"`       // The timestamp of the last status change
 	IsCheckDisabled        bool             `gorm:"default:false"`   // A flag indicating if checks are disabled for this account
-	InstallationType       InstallationType `gorm:"default:0"`
-	GameStatuses           []GameStatus     // The statuses for each game
-}
-
-type GameStatus struct {
-	gorm.Model
-	AccountID   uint
-	GameTitle   string
-	Status      Status
-	Enforcement string
-	CanAppeal   bool
-	CaseNumber  string
-	CaseStatus  string
+	InstallationType       InstallationType `gorm:"default:0"`       // The type of installation for the account.
 }
 
 type Ban struct {
@@ -60,9 +48,9 @@ type UserSettings struct {
 type InstallationType int
 
 const (
-	InstallTypeUnknown InstallationType = iota
-	InstallTypeUser
-	InstallTypeGuild
+	InstallTypeUnknown InstallationType = iota // Unknown installation type
+	InstallTypeUser                            // User installation
+	InstallTypeGuild                           // Guild installation
 )
 
 type Status string
@@ -71,8 +59,22 @@ const (
 	StatusGood          Status = "good"           // The account is in good standing.
 	StatusPermaban      Status = "permaban"       // The account has been permanently banned.
 	StatusShadowban     Status = "shadowban"      // The account has been shadowbanned.
+	StatusTempban       Status = "tempban"        // The account is temporarily banned
 	StatusUnknown       Status = "unknown"        // The status of the account is unknown.
 	StatusInvalidCookie Status = "invalid_cookie" // The account has an invalid SSO cookie.
-	StatusOpen          Status = "open"           // The account has an open case.
-	StatusClosed        Status = "closed"         // The account has a closed case.
 )
+
+type AccountStatus struct {
+	Overall Status                // The overall status of the account.
+	Games   map[string]GameStatus // A map of game titles to their status.
+}
+
+type GameStatus struct {
+	Title           string // The title of the game.
+	Status          Status // The status of the account.
+	Enforcement     string // The enforcement status of the account.
+	CanAppeal       bool   // whether the account can appeal the ban
+	CaseNumber      string // Case number of the account
+	CaseStatus      string // Case status of the account
+	DurationSeconds int    // Duration of temporary ban in seconds
+}
