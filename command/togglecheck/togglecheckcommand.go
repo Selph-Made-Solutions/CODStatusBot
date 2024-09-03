90cbf0f68bf278199e204017d7d39ef1cc763744
bradselph <Bselph512@gmail.com>
2024-09-02 23:35:41 -0400

    Update togglecheckcommand.go

--- command/togglecheck/togglecheckcommand.go
+++ command/togglecheck/togglecheckcommand.go
@@ -5,94 +5,73 @@ import (
 	"CODStatusBot/logger"
 	"CODStatusBot/models"
 	"fmt"
-	"github.com/bwmarrin/discordgo"
+	"github.com/disgoorg/disgo/bot"
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/events"
 	"strconv"
 	"strings"
 )
 
-func CommandToggleCheck(s *discordgo.Session, i *discordgo.InteractionCreate, installType models.InstallationType) {
-	var userID string
-	if i.Member != nil {
-		userID = i.Member.User.ID
-	} else if i.User != nil {
-		userID = i.User.ID
-	} else {
-		logger.Log.Error("Interaction doesn't have Member or User")
-		respondToInteraction(s, i, "An error occurred while processing your request.")
-		return
-	}
+func CommandToggleCheck(client bot.Client, event *events.ApplicationCommandInteractionCreate, installType models.InstallationType) error {
+	userID := event.User().ID
 
 	var accounts []models.Account
 	result := database.DB.Where("user_id = ?", userID).Find(&accounts)
 	if result.Error != nil {
 		logger.Log.WithError(result.Error).Error("Error fetching user accounts")
-		respondToInteraction(s, i, "Error fetching your accounts. Please try again.")
-		return
+		return respondToInteraction(event, "Error fetching your accounts. Please try again.")
 	}
 
 	if len(accounts) == 0 {
-		respondToInteraction(s, i, "You don't have any monitored accounts.")
-		return
+		return respondToInteraction(event, "You don't have any monitored accounts.")
 	}
 
-	// Create buttons for each account
-	var components []discordgo.MessageComponent
+	var components []discord.MessageComponent
 	for _, account := range accounts {
 		label := fmt.Sprintf("%s (%s - %s)", account.Title, getCheckStatus(account.IsCheckDisabled), account.LastStatus.Overall)
-		components = append(components, discordgo.Button{
+		components = append(components, discord.ButtonComponent{
 			Label:    label,
-			Style:    discordgo.PrimaryButton,
+			Style:    discord.ButtonStylePrimary,
 			CustomID: fmt.Sprintf("toggle_check_%d", account.ID),
 		})
 	}
 
-	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-		Type: discordgo.InteractionResponseChannelMessageWithSource,
-		Data: &discordgo.InteractionResponseData{
-			Content: "Select an account to toggle auto check On/Off:",
-			Flags:   discordgo.MessageFlagsEphemeral,
-			Components: []discordgo.MessageComponent{
-				discordgo.ActionsRow{
-					Components: components,
-				},
+	return event.CreateMessage(discord.MessageCreate{
+		Content: "Select an account to toggle auto check On/Off:",
+		Flags:   discord.MessageFlagEphemeral,
+		Components: []discord.MessageComponent{
+			discord.ActionRowComponent{
+				Components: components,
 			},
 		},
 	})
-
-	if err != nil {
-		logger.Log.WithError(err).Error("Error responding with account selection")
-	}
 }
 
-func HandleAccountSelection(s *discordgo.Session, i *discordgo.InteractionCreate, installType models.InstallationType) {
-	customID := i.MessageComponentData().CustomID
+func HandleAccountSelection(client bot.Client, event *events.ComponentInteractionCreate) error {
+	customID := event.Data.CustomID()
 	accountID, err := strconv.Atoi(strings.TrimPrefix(customID, "toggle_check_"))
 	if err != nil {
 		logger.Log.WithError(err).Error("Error parsing account ID")
-		respondToInteraction(s, i, "Error processing your selection. Please try again.")
-		return
+		return respondToInteraction(event, "Error processing your selection. Please try again.")
 	}
 
 	var account models.Account
 	result := database.DB.First(&account, accountID)
 	if result.Error != nil {
 		logger.Log.WithError(result.Error).Error("Error fetching account")
-		respondToInteraction(s, i, "Error: Account not found")
-		return
+		return respondToInteraction(event, "Error: Account not found")
 	}
 
-	// Toggle the IsCheckDisabled field
 	account.IsCheckDisabled = !account.IsCheckDisabled
 
 	if err := database.DB.Save(&account).Error; err != nil {
 		logger.Log.WithError(err).Error("Error saving account changes")
-		respondToInteraction(s, i, "Error toggling account checks. Please try again.")
-		return
+		return respondToInteraction(event, "Error toggling account checks. Please try again.")
 	}
 
 	status := getCheckStatus(account.IsCheckDisabled)
 	message := fmt.Sprintf("Checks for account '%s' are now %s.", account.Title, status)
-	respondToInteraction(s, i, message)
+	return respondToInteraction(event, message)
 }
 
 func getCheckStatus(isDisabled bool) string {
@@ -102,15 +81,19 @@ func getCheckStatus(isDisabled bool) string {
 	return "enabled"
 }
 
-func respondToInteraction(s *discordgo.Session, i *discordgo.InteractionCreate, message string) {
-	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-		Type: discordgo.InteractionResponseUpdateMessage,
-		Data: &discordgo.InteractionResponseData{
-			Content:    message,
-			Components: []discordgo.MessageComponent{},
-		},
-	})
-	if err != nil {
-		logger.Log.WithError(err).Error("Error responding to interaction")
+func respondToInteraction(event interface{}, message string) error {
+	switch e := event.(type) {
+	case *events.ComponentInteractionCreate:
+		return e.UpdateMessage(discord.MessageUpdate{
+			Content:    discord.NewValueOptional(message),
+			Components: []discord.MessageComponent{},
+		})
+	case *events.ApplicationCommandInteractionCreate:
+		return e.CreateMessage(discord.MessageCreate{
+			Content: message,
+			Flags:   discord.MessageFlagEphemeral,
+		})
+	default:
+		return nil
 	}
 }
