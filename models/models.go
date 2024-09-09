75fbc1d82956581af3e986a57783c7a8c97b86e4
bradselph <Bselph512@gmail.com>
2024-09-09 08:04:30 -0400

    Update models.go

--- models/models.go
+++ models/models.go
@@ -21,7 +21,7 @@ type Account struct {
 	LastCookieCheck        int64  `gorm:"default:0"`       // The timestamp of the last cookie check for permanently banned accounts
 	LastStatusChange       int64  `gorm:"default:0"`       // The timestamp of the last status change
 	IsCheckDisabled        bool   `gorm:"default:false"`   // A flag indicating if checks are disabled for this account
-	SSOCookieExpiration    int64
+	SSOCookieExpiration    int64 //
 }
 
 type UserSettings struct {
