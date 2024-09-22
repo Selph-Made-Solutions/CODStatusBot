9d41c5cd479261097e87651e96878e2c0e83f00b
bradselph <Bselph512@gmail.com>
2024-09-22 12:39:25 -0400

    Update bot.go

--- bot/bot.go
+++ bot/bot.go
@@ -1,6 +1,10 @@
 package bot
 
 import (
+	"errors"
+	"os"
+	"strings"
+
 	"CODStatusBot/command"
 	"CODStatusBot/command/accountage"
 	"CODStatusBot/command/accountlogs"
@@ -16,29 +20,11 @@ import (
 	"CODStatusBot/logger"
 	"CODStatusBot/models"
 	"CODStatusBot/services"
-	"errors"
-	"os"
-	"strings"
-	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 var discord *discordgo.Session
-
-func init() {
-	// Initialize the database connection
-	err := database.Databaselogin()
-	if err != nil {
-		logger.Log.WithError(err).Fatal("Failed to initialize database connection")
-	}
-
-	// Create or update the UserSettings table
-	err = database.DB.AutoMigrate(&models.UserSettings{})
-	if err != nil {
-		logger.Log.WithError(err).Fatal("Failed to create or update UserSettings table")
-	}
-}
 
 func StartBot() (*discordgo.Session, error) {
 	envToken := os.Getenv("DISCORD_TOKEN")
@@ -81,12 +67,7 @@ func StartBot() (*discordgo.Session, error) {
 		}
 	})
 
-	// Add new event handler for GUILD_DELETE
-	discord.AddHandler(handleGuildDelete)
-
 	go services.CheckAccounts(discord)
-	go periodicUserCheck(discord) // Start periodic check for user applications
-
 	return discord, nil
 }
 
@@ -141,105 +122,16 @@ func handleMessageComponent(s *discordgo.Session, i *discordgo.InteractionCreate
 	}
 }
 
-func handleGuildDelete(s *discordgo.Session, g *discordgo.GuildDelete) {
-	logger.Log.Infof("Bot removed from guild: %s", g.ID)
-
-	// Update all UserSettings for this guild
-	err := database.DB.Model(&models.UserSettings{}).
-		Where("install_type = ? AND user_id IN (SELECT user_id FROM accounts WHERE channel_id LIKE ?)",
-			"guild", g.ID+"%").
-		Updates(map[string]interface{}{"is_bot_installed": false}).Error
-
+func init() {
+	// Initialize the database connection
+	err := database.Databaselogin()
 	if err != nil {
-		logger.Log.WithError(err).Error("Failed to update UserSettings after guild removal")
+		logger.Log.WithError(err).Fatal("Failed to initialize database connection")
 	}
 
-	// Disable checks for all accounts in this guild
-	err = database.DB.Model(&models.Account{}).
-		Where("channel_id LIKE ?", g.ID+"%").
-		Update("is_check_disabled", true).Error
-
+	// Create or update the UserSettings table
+	err = database.DB.AutoMigrate(&models.UserSettings{})
 	if err != nil {
-		logger.Log.WithError(err).Error("Failed to disable checks for accounts after guild removal")
-	}
-}
-
-func periodicUserCheck(s *discordgo.Session) {
-	ticker := time.NewTicker(24 * time.Hour) // Check once a day
-	for range ticker.C {
-		var userSettings []models.UserSettings
-		err := database.DB.Where("install_type = ? AND is_bot_installed = ?", "user", true).Find(&userSettings).Error
-		if err != nil {
-			logger.Log.WithError(err).Error("Failed to fetch user applications for periodic check")
-			continue
-		}
-
-		for _, settings := range userSettings {
-			_, err := s.UserChannelCreate(settings.UserID)
-			if err != nil {
-				logger.Log.WithError(err).Infof("Failed to create DM channel with user %s, marking as uninstalled", settings.UserID)
-				settings.IsBotInstalled = false
-				database.DB.Save(&settings)
-
-				// Disable checks for all accounts of this user
-				err = database.DB.Model(&models.Account{}).
-					Where("user_id = ?", settings.UserID).
-					Update("is_check_disabled", true).Error
-
-				if err != nil {
-					logger.Log.WithError(err).Error("Failed to disable checks for accounts after user removal")
-				}
-			}
-		}
-	}
-}
-
-func HandleCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	// Check if the user has seen the announcement
-	var userID string
-	if i.Member != nil {
-		userID = i.Member.User.ID
-	} else if i.User != nil {
-		userID = i.User.ID
-	} else {
-		logger.Log.Error("Interaction doesn't have Member or User")
-		return
-	}
-
-	var userSettings models.UserSettings
-	result := database.DB.Where(models.UserSettings{UserID: userID}).FirstOrCreate(&userSettings)
-	if result.Error != nil {
-		logger.Log.WithError(result.Error).Error("Error getting user settings")
-	} else {
-		if !userSettings.IsBotInstalled {
-			// Re-enable the bot for this user
-			userSettings.IsBotInstalled = true
-			if err := database.DB.Save(&userSettings).Error; err != nil {
-				logger.Log.WithError(err).Error("Error updating user settings after re-enabling bot")
-			}
-
-			// Re-enable checks for all accounts of this user
-			err := database.DB.Model(&models.Account{}).
-				Where("user_id = ?", userID).
-				Update("is_check_disabled", false).Error
-
-			if err != nil {
-				logger.Log.WithError(err).Error("Failed to re-enable checks for accounts after user interaction")
-			}
-		}
-
-		if !userSettings.HasSeenAnnouncement {
-			// Send the announcement to the user
-			if err := services.SendGlobalAnnouncement(s, userID); err != nil {
-				logger.Log.WithError(err).Error("Error sending announcement to user")
-			}
-		}
-	}
-
-	// Continue with regular command handling
-	if h, ok := command.Handlers[i.ApplicationCommandData().Name]; ok {
-		h(s, i)
-	} else {
-		logger.Log.Warnf("Unhandled command: %s", i.ApplicationCommandData().Name)
+		logger.Log.WithError(err).Fatal("Failed to create or update UserSettings table")
 	}
 }
