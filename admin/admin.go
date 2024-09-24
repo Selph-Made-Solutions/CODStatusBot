184ef49c389a87a485c4591f5963530f4590621e
bradselph <Bselph512@gmail.com>
2024-09-24 08:05:14 -0400

    Update admin.go

--- admin/admin.go
+++ admin/admin.go
@@ -14,6 +14,28 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Stats struct {
+	TotalAccounts          int
+	ActiveAccounts         int
+	ChecksLastHour         int
+	ChecksLast24Hours      int
+	BannedAccounts         int
+	TotalUsers             int
+	TotalBans              int
+	RecentBans             int
+	AverageChecksPerDay    float64
+	MostCheckedAccount     string
+	LeastCheckedAccount    string
+	TotalNotifications     int
+	RecentNotifications    int
+	UsersWithCustomAPIKey  int
+	AverageAccountsPerUser float64
+	OldestAccount          time.Time
+	NewestAccount          time.Time
+	TotalShadowbans        int
+	TotalTempbans          int
+}
+
 func StartAdminPanel() {
 	r := mux.NewRouter()
 
@@ -66,15 +88,6 @@ func dashboardHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-type Stats struct {
-	TotalAccounts     int
-	ActiveAccounts    int
-	BannedAccounts    int
-	TotalUsers        int
-	ChecksLastHour    int
-	ChecksLast24Hours int
-}
-
 func getStats() (Stats, error) {
 	var stats Stats
 	var err error
@@ -89,22 +102,62 @@ func getStats() (Stats, error) {
 		return stats, err
 	}
 
-	stats.BannedAccounts, err = getBannedAccounts()
-	if err != nil {
-		return stats, err
-	}
-
 	stats.TotalUsers, err = getTotalUsers()
 	if err != nil {
 		return stats, err
 	}
 
-	stats.ChecksLastHour, err = getChecksInTimeRange(time.Hour)
+	stats.ChecksLast24Hours, err = getChecksInTimeRange(24 * time.Hour)
 	if err != nil {
 		return stats, err
 	}
 
-	stats.ChecksLast24Hours, err = getChecksInTimeRange(24 * time.Hour)
+	stats.TotalBans, err = getTotalBans()
+	if err != nil {
+		return stats, err
+	}
+
+	stats.RecentBans, err = getRecentBans(24 * time.Hour)
+	if err != nil {
+		return stats, err
+	}
+
+	stats.AverageChecksPerDay, err = getAverageChecksPerDay()
+	if err != nil {
+		return stats, err
+	}
+
+	stats.TotalNotifications, err = getTotalNotifications()
+	if err != nil {
+		return stats, err
+	}
+
+	stats.RecentNotifications, err = getRecentNotifications(24 * time.Hour)
+	if err != nil {
+		return stats, err
+	}
+
+	stats.UsersWithCustomAPIKey, err = getUsersWithCustomAPIKey()
+	if err != nil {
+		return stats, err
+	}
+
+	stats.AverageAccountsPerUser, err = getAverageAccountsPerUser()
+	if err != nil {
+		return stats, err
+	}
+
+	stats.OldestAccount, stats.NewestAccount, err = getAccountAgeRange()
+	if err != nil {
+		return stats, err
+	}
+
+	stats.TotalShadowbans, err = getTotalBansByType(models.StatusShadowban)
+	if err != nil {
+		return stats, err
+	}
+
+	stats.TotalTempbans, err = getTotalBansByType(models.StatusTempban)
 	if err != nil {
 		return stats, err
 	}
@@ -124,12 +177,6 @@ func getActiveAccounts() (int, error) {
 	return int(count), err
 }
 
-func getBannedAccounts() (int, error) {
-	var count int64
-	err := database.DB.Model(&models.Account{}).Where("is_permabanned = ?", true).Count(&count).Error
-	return int(count), err
-}
-
 func getTotalUsers() (int, error) {
 	var count int64
 	err := database.DB.Model(&models.UserSettings{}).Count(&count).Error
@@ -140,5 +187,66 @@ func getChecksInTimeRange(duration time.Duration) (int, error) {
 	var count int64
 	timeThreshold := time.Now().Add(-duration).Unix()
 	err := database.DB.Model(&models.Account{}).Where("last_check > ?", timeThreshold).Count(&count).Error
+	return int(count), err
+}
+func getTotalBans() (int, error) {
+	var count int64
+	err := database.DB.Model(&models.Ban{}).Count(&count).Error
+	return int(count), err
+}
+
+func getRecentBans(duration time.Duration) (int, error) {
+	var count int64
+	err := database.DB.Model(&models.Ban{}).Where("created_at > ?", time.Now().Add(-duration)).Count(&count).Error
+	return int(count), err
+}
+
+func getAverageChecksPerDay() (float64, error) {
+	var result struct {
+		AvgChecks float64
+	}
+	err := database.DB.Model(&models.Account{}).Select("AVG(checks_count) as avg_checks").Scan(&result).Error
+	return result.AvgChecks, err
+}
+
+func getTotalNotifications() (int, error) {
+	var count int64
+	err := database.DB.Model(&models.Account{}).Where("last_notification > 0").Count(&count).Error
+	return int(count), err
+}
+
+func getRecentNotifications(duration time.Duration) (int, error) {
+	var count int64
+	err := database.DB.Model(&models.Account{}).Where("last_notification > ?", time.Now().Add(-duration).Unix()).Count(&count).Error
+	return int(count), err
+}
+
+func getUsersWithCustomAPIKey() (int, error) {
+	var count int64
+	err := database.DB.Model(&models.UserSettings{}).Where("captcha_api_key != ''").Count(&count).Error
+	return int(count), err
+}
+
+func getAverageAccountsPerUser() (float64, error) {
+	var result struct {
+		AvgAccounts float64
+	}
+	err := database.DB.Model(&models.Account{}).Select("COUNT(DISTINCT id) / COUNT(DISTINCT user_id) as avg_accounts").Scan(&result).Error
+	return result.AvgAccounts, err
+}
+
+func getAccountAgeRange() (time.Time, time.Time, error) {
+	var oldestAccount, newestAccount models.Account
+	err := database.DB.Order("created ASC").First(&oldestAccount).Error
+	if err != nil {
+		return time.Time{}, time.Time{}, err
+	}
+	err = database.DB.Order("created DESC").First(&newestAccount).Error
+	return time.Unix(oldestAccount.Created, 0), time.Unix(newestAccount.Created, 0), err
+}
+
+func getTotalBansByType(banType models.Status) (int, error) {
+	var count int64
+	err := database.DB.Model(&models.Ban{}).Where("status = ?", banType).Count(&count).Error
 	return int(count), err
 }
