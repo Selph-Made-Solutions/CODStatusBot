202dfa96ce14e2a6bf882f194ae9923308f97de1
bradselph <Bselph512@gmail.com>
2024-09-26 03:00:45 -0400

    Update bot.go

--- bot/bot.go
+++ bot/bot.go
@@ -6,21 +6,16 @@ import (
 	"strings"
 
 	"CODStatusBot/command"
-	"CODStatusBot/command/accountage"
 	"CODStatusBot/command/accountlogs"
 	"CODStatusBot/command/addaccount"
-	"CODStatusBot/command/checknow"
 	"CODStatusBot/command/feedback"
 	"CODStatusBot/command/removeaccount"
 	"CODStatusBot/command/setcaptchaservice"
 	"CODStatusBot/command/setcheckinterval"
 	"CODStatusBot/command/togglecheck"
 	"CODStatusBot/command/updateaccount"
-	"CODStatusBot/database"
+	"CODStatusBot/errorhandler"
 	"CODStatusBot/logger"
-	"CODStatusBot/models"
-	"CODStatusBot/services"
-
 	"github.com/bwmarrin/discordgo"
 )
 
@@ -29,112 +24,119 @@ var discord *discordgo.Session
 func StartBot() (*discordgo.Session, error) {
 	envToken := os.Getenv("DISCORD_TOKEN")
 	if envToken == "" {
-		err := errors.New("DISCORD_TOKEN environment variable not set")
-		logger.Log.WithError(err).WithField("env", "DISCORD_TOKEN").Error()
-		return nil, err
+		return nil, errorhandler.NewValidationError(errors.New("DISCORD_TOKEN not set"), "Discord token")
 	}
 
 	var err error
 	discord, err = discordgo.New("Bot " + envToken)
 	if err != nil {
-		logger.Log.WithError(err).WithField("Bot startup", "Token").Error()
-		return nil, err
+		return nil, errorhandler.NewAPIError(err, "Discord")
 	}
 
 	err = discord.Open()
 	if err != nil {
-		logger.Log.WithError(err).WithField("Bot startup", "Opening Session").Error()
-		return nil, err
+		return nil, errorhandler.NewAPIError(err, "Discord")
 	}
 
 	err = discord.UpdateWatchStatus(0, "the Status of your Accounts so you dont have to.")
 	if err != nil {
-		logger.Log.WithError(err).WithField("Bot startup", "Setting Presence Status").Error()
-		return nil, err
+		return nil, errorhandler.NewDiscordError(err, "Discord")
 	}
 
 	command.RegisterCommands(discord)
 	logger.Log.Info("Registering global commands")
 
-	discord.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		switch i.Type {
-		case discordgo.InteractionApplicationCommand:
-			command.HandleCommand(s, i)
-		case discordgo.InteractionModalSubmit:
-			handleModalSubmit(s, i)
-		case discordgo.InteractionMessageComponent:
-			handleMessageComponent(s, i)
+	discord.AddHandler(
+		func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			switch i.Type {
+			case discordgo.InteractionApplicationCommand:
+				command.HandleCommand(s, i)
+			case discordgo.InteractionModalSubmit:
+				handleModalSubmit(s, i)
+			case discordgo.InteractionMessageComponent:
+				handleMessageComponent(s, i)
+			}
 		}
-	})
+	)
 
 	go services.CheckAccounts(discord)
 	return discord, nil
 }
-
 func handleModalSubmit(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	customID := i.ModalSubmitData().CustomID
+	var err error
 	switch {
 	case customID == "set_captcha_service_modal":
-		setcaptchaservice.HandleModalSubmit(s, i)
+		err = setcaptchaservice.HandleModalSubmit(s, i)
 	case customID == "add_account_modal":
-		addaccount.HandleModalSubmit(s, i)
+		err = addaccount.HandleModalSubmit(s, i)
 	case strings.HasPrefix(customID, "update_account_modal_"):
-		updateaccount.HandleModalSubmit(s, i)
+		err = updateaccount.HandleModalSubmit(s, i)
 	case customID == "set_check_interval_modal":
-		setcheckinterval.HandleModalSubmit(s, i)
+		err = setcheckinterval.HandleModalSubmit(s, i)
 	default:
-		logger.Log.WithField("customID", customID).Error("Unknown modal submission")
+		err = errorhandler.NewValidationError(fmt.Errorf("unknown modal submission: %s", customID), "modal submission")
+	}
+
+	if err != nil {
+		userMsg, _ := errorhandler.HandleError(err)
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: userMsg,
+				Flags:   discordgo.MessageFlagsEphemeral,
+			},
+		})
 	}
 }
 
 func handleMessageComponent(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	customID := i.MessageComponentData().CustomID
+	var err error
 	switch {
 	case strings.HasPrefix(customID, "feedback_"):
-		feedback.HandleFeedbackChoice(s, i)
-		logger.Log.Info("Handling feedback choice")
+		err = feedback.HandleFeedbackChoice(s, i)
+		//logger.Log.Info("Handling feedback choice")
 	case strings.HasPrefix(customID, "account_age_"):
-		accountage.HandleAccountSelection(s, i)
-		logger.Log.Info("Handling account age selection")
+		err = accountage.HandleAccountSelection(s, i)
+		//logger.Log.Info("Handling account age selection")
 	case strings.HasPrefix(customID, "account_logs_"):
-		accountlogs.HandleAccountSelection(s, i)
-		logger.Log.Info("Handling account logs selection")
+		err = accountlogs.HandleAccountSelection(s, i)
+		//logger.Log.Info("Handling account logs selection")
 	case customID == "account_logs_all":
-		accountlogs.HandleAccountSelection(s, i)
-		logger.Log.Info("Handling account logs selection")
+		err = accountlogs.HandleAccountSelection(s, i)
+		//logger.Log.Info("Handling account logs selection")
 	case strings.HasPrefix(customID, "update_account_"):
-		updateaccount.HandleAccountSelection(s, i)
-		logger.Log.Info("Handling update account selection")
+		err = updateaccount.HandleAccountSelection(s, i)
+		//logger.Log.Info("Handling update account selection")
 	case strings.HasPrefix(customID, "remove_account_"):
-		removeaccount.HandleAccountSelection(s, i)
-		logger.Log.Info("Handling remove account selection")
+		err = removeaccount.HandleAccountSelection(s, i)
+		//logger.Log.Info("Handling remove account selection")
 	case customID == "cancel_remove" || strings.HasPrefix(customID, "confirm_remove_"):
-		removeaccount.HandleConfirmation(s, i)
-		logger.Log.Info("Handling remove account confirmation")
+		err = removeaccount.HandleConfirmation(s, i)
+		//logger.Log.Info("Handling remove account confirmation")
 	case strings.HasPrefix(customID, "check_now_"):
-		checknow.HandleAccountSelection(s, i)
-		logger.Log.Info("Handling check now selection")
+		err = checknow.HandleAccountSelection(s, i)
+		//logger.Log.Info("Handling check now selection")
 	case strings.HasPrefix(customID, "toggle_check_"):
-		togglecheck.HandleAccountSelection(s, i)
-		logger.Log.Info("Handling toggle check selection")
+		err = togglecheck.HandleAccountSelection(s, i)
+		//logger.Log.Info("Handling toggle check selection")
 	case strings.HasPrefix(customID, "confirm_reenable_") || customID == "cancel_reenable":
-		togglecheck.HandleConfirmation(s, i)
-		logger.Log.Info("Handling toggle check confirmation")
+		err = togglecheck.HandleConfirmation(s, i)
+		//logger.Log.Info("Handling toggle check confirmation")
 	default:
-		logger.Log.WithField("customID", customID).Error("Unknown message component interaction")
-	}
-}
-
-func init() {
-	// Initialize the database connection
-	err := database.Databaselogin()
-	if err != nil {
-		logger.Log.WithError(err).Fatal("Failed to initialize database connection")
+		err = errorhandler.NewValidationError(fmt.Errorf("unknown message component interaction: %s", customID), "message component")
+		//logger.Log.WithField("customID", customID).Error("Unknown message component interaction")
 	}
 
-	// Create or update the UserSettings table
-	err = database.DB.AutoMigrate(&models.UserSettings{})
 	if err != nil {
-		logger.Log.WithError(err).Fatal("Failed to create or update UserSettings table")
+		userMsg, _ := errorhandler.HandleError(err)
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: userMsg,
+				Flags:   discordgo.MessageFlagsEphemeral,
+			},
+		})
 	}
 }
