86e876b292468fada501428d38fb400b3f86b6b5
bradselph <Bselph512@gmail.com>
2024-09-26 01:57:16 -0400

    Update accountagecommand.go

--- command/accountage/accountagecommand.go
+++ command/accountage/accountagecommand.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"CODStatusBot/database"
+	"CODStatusBot/errorhandler"
 	"CODStatusBot/logger"
 	"CODStatusBot/models"
 	"CODStatusBot/services"
@@ -15,22 +16,16 @@ import (
 )
 
 func CommandAccountAge(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	var userID string
-	if i.Member != nil {
-		userID = i.Member.User.ID
-	} else if i.User != nil {
-		userID = i.User.ID
-	} else {
-		logger.Log.Error("Interaction doesn't have Member or User")
-		respondToInteraction(s, i, "An error occurred while processing your request.")
+	userID, err := getUserID(i)
+	if err != nil {
+		handleInteractionError(s, i, errorhandler.NewValidationError(err, "user ID"))
 		return
 	}
 
 	var accounts []models.Account
 	result := database.DB.Where("user_id = ?", userID).Find(&accounts)
 	if result.Error != nil {
-		logger.Log.WithError(result.Error).Error("Error fetching user accounts")
-		respondToInteraction(s, i, "Error fetching your accounts. Please try again.")
+		handleInteractionError(s, i, errorhandler.NewDatabaseError(result.Error, "fetching user accounts"))
 		return
 	}
 
@@ -39,9 +34,10 @@ func CommandAccountAge(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		return
 	}
 
-	// Create buttons for each account
-	var components []discordgo.MessageComponent
-	var currentRow []discordgo.MessageComponent
+	var (
+		components []discordgo.MessageComponent
+		currentRow []discordgo.MessageComponent
+	)
 
 	for _, account := range accounts {
 		currentRow = append(currentRow, discordgo.Button{
@@ -56,12 +52,11 @@ func CommandAccountAge(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		}
 	}
 
-	// Add the last row if it is not empty
 	if len(currentRow) > 0 {
 		components = append(components, discordgo.ActionsRow{Components: currentRow})
 	}
 
-	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+	err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
 			Content:    "Select an account to check its age:",
@@ -70,7 +65,7 @@ func CommandAccountAge(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		},
 	})
 	if err != nil {
-		logger.Log.WithError(err).Error("Error responding with account selection")
+		handleInteractionError(s, i, errorhandler.NewAPIError(err, "Discord"))
 	}
 }
 
@@ -78,30 +73,27 @@ func HandleAccountSelection(s *discordgo.Session, i *discordgo.InteractionCreate
 	customID := i.MessageComponentData().CustomID
 	accountID, err := strconv.Atoi(strings.TrimPrefix(customID, "account_age_"))
 	if err != nil {
-		logger.Log.WithError(err).Error("Error parsing account ID")
-		respondToInteraction(s, i, "Error processing your selection. Please try again.")
+		handleInteractionError(s, i, errorhandler.NewValidationError(err, "account ID"))
 		return
 	}
 
 	var account models.Account
 	result := database.DB.First(&account, accountID)
 	if result.Error != nil {
-		logger.Log.WithError(result.Error).Error("Error fetching account")
-		respondToInteraction(s, i, "Error: Account not found or you don't have permission to check its age.")
+		handleInteractionError(s, i, errorhandler.NewDatabaseError(result.Error, "fetching account"))
 		return
 	}
 
 	if !services.VerifySSOCookie(account.SSOCookie) {
-		account.IsExpiredCookie = true // Update account's IsExpiredCookie flag
+		account.IsExpiredCookie = true
 		database.DB.Save(&account)
-		respondToInteraction(s, i, "Invalid SSOCookie. Account's cookie status updated.")
+		respondToInteraction(s, i, "The SSO cookie for this account has expired. Please update it using the /updateaccount command.")
 		return
 	}
 
 	years, months, days, createdEpoch, err := services.CheckAccountAge(account.SSOCookie)
 	if err != nil {
-		logger.Log.WithError(err).Errorf("Error checking account age for account %s", account.Title)
-		respondToInteraction(s, i, "There was an error checking the account age.")
+		handleInteractionError(s, i, errorhandler.NewAPIError(err, "Activision"))
 		return
 	}
 
@@ -140,9 +132,13 @@ func HandleAccountSelection(s *discordgo.Session, i *discordgo.InteractionCreate
 		},
 	})
 	if err != nil {
-		logger.Log.WithError(err).Error("Error responding to interaction with account age")
-		respondToInteraction(s, i, "Error displaying account age. Please try again.")
+		handleInteractionError(s, i, errorhandler.NewAPIError(err, "Discord"))
 	}
+}
+
+func handleInteractionError(s *discordgo.Session, i *discordgo.InteractionCreate, err error) {
+	userMsg, _ := errorhandler.HandleError(err)
+	respondToInteraction(s, i, userMsg)
 }
 
 func respondToInteraction(s *discordgo.Session, i *discordgo.InteractionCreate, content string) {
@@ -156,4 +152,14 @@ func respondToInteraction(s *discordgo.Session, i *discordgo.InteractionCreate, 
 	if err != nil {
 		logger.Log.WithError(err).Error("Error responding to interaction")
 	}
+}
+
+func getUserID(i *discordgo.InteractionCreate) (string, error) {
+	if i.Member != nil && i.Member.User != nil {
+		return i.Member.User.ID, nil
+	}
+	if i.User != nil {
+		return i.User.ID, nil
+	}
+	return "", fmt.Errorf("unable to determine user ID")
 }
