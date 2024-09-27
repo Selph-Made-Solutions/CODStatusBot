25c9b920d9ea1c234110a84e44fcc5e4cc0a0193
bradselph <Bselph512@gmail.com>
2024-09-26 23:05:26 -0400

    Update removeaccountcommand.go

--- command/removeaccount/removeaccountcommand.go
+++ command/removeaccount/removeaccountcommand.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"CODStatusBot/database"
+	"CODStatusBot/errorhandler"
 	"CODStatusBot/logger"
 	"CODStatusBot/models"
 
@@ -13,22 +14,16 @@ import (
 )
 
 func CommandRemoveAccount(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	var userID string
-	if i.Member != nil {
-		userID = i.Member.User.ID
-	} else if i.User != nil {
-		userID = i.User.ID
-	} else {
-		logger.Log.Error("Interaction doesn't have Member or User")
-		respondToInteraction(s, i, "An error occurred while processing your request.")
+	userID, err := getUserID(i)
+	if err != nil {
+		handleError(s, i, err)
 		return
 	}
 
 	var accounts []models.Account
 	result := database.DB.Where("user_id = ?", userID).Find(&accounts)
 	if result.Error != nil {
-		logger.Log.WithError(result.Error).Error("Error fetching user accounts")
-		respondToInteraction(s, i, "Error fetching your accounts. Please try again.")
+		handleError(s, i, errorhandler.NewDatabaseError(result.Error, "fetching user accounts"))
 		return
 	}
 
@@ -58,8 +53,8 @@ func CommandRemoveAccount(s *discordgo.Session, i *discordgo.InteractionCreate) 
 	if len(currentRow) > 0 {
 		components = append(components, discordgo.ActionsRow{Components: currentRow})
 	}
-	// Send a message with account buttons
-	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+
+	err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
 			Content:    "Select an account to remove:",
@@ -68,7 +63,7 @@ func CommandRemoveAccount(s *discordgo.Session, i *discordgo.InteractionCreate) 
 		},
 	})
 	if err != nil {
-		logger.Log.WithError(err).Error("Error responding with account selection")
+		handleError(s, i, errorhandler.NewDiscordError(err, "sending account selection message"))
 	}
 }
 
@@ -76,16 +71,14 @@ func HandleAccountSelection(s *discordgo.Session, i *discordgo.InteractionCreate
 	customID := i.MessageComponentData().CustomID
 	accountID, err := strconv.Atoi(strings.TrimPrefix(customID, "remove_account_"))
 	if err != nil {
-		logger.Log.WithError(err).Error("Error parsing account ID")
-		respondToInteraction(s, i, "Error processing your selection. Please try again.")
+		handleError(s, i, errorhandler.NewValidationError(err, "account ID"))
 		return
 	}
 
 	var account models.Account
 	result := database.DB.First(&account, accountID)
 	if result.Error != nil {
-		logger.Log.WithError(result.Error).Error("Error fetching account")
-		respondToInteraction(s, i, "Error: Account not found or you don't have permission to remove it.")
+		handleError(s, i, errorhandler.NewDatabaseError(result.Error, "fetching account"))
 		return
 	}
 
@@ -113,8 +106,7 @@ func HandleAccountSelection(s *discordgo.Session, i *discordgo.InteractionCreate
 		},
 	})
 	if err != nil {
-		logger.Log.WithError(err).Error("Error showing confirmation buttons")
-		respondToInteraction(s, i, "An error occurred. Please try again.")
+		handleError(s, i, errorhandler.NewDiscordError(err, "sending confirmation message"))
 	}
 }
 
@@ -128,16 +120,14 @@ func HandleConfirmation(s *discordgo.Session, i *discordgo.InteractionCreate) {
 
 	accountID, err := strconv.Atoi(strings.TrimPrefix(customID, "confirm_remove_"))
 	if err != nil {
-		logger.Log.WithError(err).Error("Error parsing account ID")
-		respondToInteraction(s, i, "Error processing your confirmation. Please try again.")
+		handleError(s, i, errorhandler.NewValidationError(err, "account ID"))
 		return
 	}
 
 	var account models.Account
 	result := database.DB.First(&account, accountID)
 	if result.Error != nil {
-		logger.Log.WithError(result.Error).Error("Error fetching account")
-		respondToInteraction(s, i, "Error: Account not found or you don't have permission to remove it.")
+		handleError(s, i, errorhandler.NewDatabaseError(result.Error, "fetching account"))
 		return
 	}
 
@@ -147,27 +137,29 @@ func HandleConfirmation(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	// Delete associated bans
 	if err := tx.Where("account_id = ?", account.ID).Delete(&models.Ban{}).Error; err != nil {
 		tx.Rollback()
-		logger.Log.WithError(err).Error("Error deleting associated bans")
-		respondToInteraction(s, i, "Error removing account. Please try again.")
+		handleError(s, i, errorhandler.NewDatabaseError(err, "deleting associated bans"))
 		return
 	}
 
 	// Delete the account
 	if err := tx.Delete(&account).Error; err != nil {
 		tx.Rollback()
-		logger.Log.WithError(err).Error("Error deleting account")
-		respondToInteraction(s, i, "Error removing account. Please try again.")
+		handleError(s, i, errorhandler.NewDatabaseError(err, "deleting account"))
 		return
 	}
 
 	// Commit the transaction
 	if err := tx.Commit().Error; err != nil {
-		logger.Log.WithError(err).Error("Error committing transaction")
-		respondToInteraction(s, i, "Error removing account. Please try again.")
+		handleError(s, i, errorhandler.NewDatabaseError(err, "committing transaction"))
 		return
 	}
 
 	respondToInteraction(s, i, fmt.Sprintf("Account '%s' has been successfully removed from the database.", account.Title))
+}
+
+func handleError(s *discordgo.Session, i *discordgo.InteractionCreate, err error) {
+	userMsg, _ := errorhandler.HandleError(err)
+	respondToInteraction(s, i, userMsg)
 }
 
 func respondToInteraction(s *discordgo.Session, i *discordgo.InteractionCreate, message string) {
@@ -175,7 +167,7 @@ func respondToInteraction(s *discordgo.Session, i *discordgo.InteractionCreate, 
 		Type: discordgo.InteractionResponseUpdateMessage,
 		Data: &discordgo.InteractionResponseData{
 			Content:    message,
-			Components: []discordgo.MessageComponent{}, // Remove all components
+			Components: []discordgo.MessageComponent{},
 		},
 	})
 	if err != nil {
@@ -190,5 +182,5 @@ func getUserID(i *discordgo.InteractionCreate) (string, error) {
 	if i.User != nil {
 		return i.User.ID, nil
 	}
-	return "", fmt.Errorf("unable to determine user ID")
+	return "", errorhandler.NewValidationError(fmt.Errorf("unable to determine user ID"), "user identification")
 }
