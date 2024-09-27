9a5d6edf7624fc0e05cade7706e2f229a5c7abba
bradselph <Bselph512@gmail.com>
2024-09-27 00:07:35 -0400

    Update admin_notifications.go

--- admin/admin_notifications.go
+++ admin/admin_notifications.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
 	"os"
+	"time"
 )
 
 var adminSession *discordgo.Session
@@ -28,8 +29,45 @@ func NotifyAdmin(message string) {
 		return
 	}
 
-	_, _ = adminSession.ChannelMessageSend(channel.ID, fmt.Sprintf("Admin Notification: %s", message))
+	embed := &discordgo.MessageEmbed{
+		Title:       "Admin Notification",
+		Description: message,
+		Color:       0xFF0000, // Red color for admin notifications
+		Timestamp:   time.Now().Format(time.RFC3339),
+	}
+
+	_, _ = adminSession.ChannelMessageSendEmbed(channel.ID, embed)
 }
 
-// Use this function for important error notifications
-// Example: admin.NotifyAdmin(fmt.Sprintf("Critical error: %v", err))
+func NotifyAdminError(errorType, accountTitle string, accountID uint, userID string, errorMessage string) {
+	adminID := os.Getenv("DEVELOPER_ID")
+	if adminID == "" {
+		return
+	}
+
+	channel, err := adminSession.UserChannelCreate(adminID)
+	if err != nil {
+		return
+	}
+
+	embed := &discordgo.MessageEmbed{
+		Title:       fmt.Sprintf("%s Error", errorType),
+		Description: fmt.Sprintf("An error occurred for account '%s' (ID: %d)", accountTitle, accountID),
+		Color:       0xFF0000, // Red color for errors
+		Fields: []*discordgo.MessageEmbedField{
+			{
+				Name:   "User ID",
+				Value:  userID,
+				Inline: true,
+			},
+			{
+				Name:   "Error Message",
+				Value:  errorMessage,
+				Inline: false,
+			},
+		},
+		Timestamp: time.Now().Format(time.RFC3339),
+	}
+
+	_, _ = adminSession.ChannelMessageSendEmbed(channel.ID, embed)
+}
