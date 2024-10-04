9073c6e4ba74a409ae751ee1c090979e05495cf0
bradselph <Bselph512@gmail.com>
2024-10-04 13:40:52 -0400

    Update admin.go

--- admin/admin.go
+++ admin/admin.go
@@ -88,7 +88,10 @@ func StatsHandler(w http.ResponseWriter, r *http.Request) {
 
 	stats := GetCachedStats()
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(stats)
+	err := json.NewEncoder(w).Encode(stats)
+	if err != nil {
+		return
+	}
 }
 
 func DashboardHandler(w http.ResponseWriter, r *http.Request) {
