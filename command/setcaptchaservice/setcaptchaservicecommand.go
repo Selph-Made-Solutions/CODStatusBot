bf66bdbbdc26213e77a1098d54388f0fa6e22a4a
bradselph <Bselph512@gmail.com>
2024-10-28 00:24:40 -0400

    Update setcaptchaservicecommand.go

--- command/setcaptchaservice/setcaptchaservicecommand.go
+++ command/setcaptchaservice/setcaptchaservicecommand.go
@@ -7,11 +7,11 @@ import (
 	"time"
 
 	"github.com/bradselph/CODStatusBot/database"
-	"github.com/bradselph/CODStatusBot/discordgo"
 	"github.com/bradselph/CODStatusBot/logger"
 	"github.com/bradselph/CODStatusBot/models"
 	"github.com/bradselph/CODStatusBot/services"
 	"github.com/bradselph/CODStatusBot/utils"
+	"github.com/bwmarrin/discordgo"
 )
 
 func isServiceEnabled(provider string) bool {
