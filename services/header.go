da26b7b103a1f3d8760baee11de6a27527b0f510
bradselph <Bselph512@gmail.com>
2024-10-28 08:34:35 -0400

    Update header.go

--- services/header.go
+++ services/header.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 )
 
-// GenerateHeaders generates a map of headers for HTTP requests.
-// It includes the SSO cookie for authentication.
 func GenerateHeaders(ssoCookie string) map[string]string {
 	return map[string]string{
 		"user-agent":     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
@@ -15,8 +13,6 @@ func GenerateHeaders(ssoCookie string) map[string]string {
 	}
 }
 
-// GeneratePostHeaders generates a map of headers for HTTP POST requests.
-// It includes the SSO cookie, Content-Type and x-requested-with headers.
 func GeneratePostHeaders(ssoCookie string) map[string]string {
 	headers := GenerateHeaders(ssoCookie)
 	headers["Content-Type"] = "application/x-www-form-urlencoded"
