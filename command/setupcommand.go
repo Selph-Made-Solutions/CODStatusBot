0852d1f45306f40493bd46442442f9d9b5218bd3
bradselph <Bselph512@gmail.com>
2024-10-28 05:50:29 -0400

    Revert "Update setupcommand.go"
    This reverts commit 1f2b7f2582d784f9106a5e52e61a37187bc5f68c.

--- command/setupcommand.go
+++ command/setupcommand.go
@@ -153,12 +153,8 @@ func RegisterCommands(s *discordgo.Session) error {
 	Handlers["remove_account"] = removeaccount.HandleAccountSelection
 	Handlers["check_now"] = checknow.HandleAccountSelection
 	Handlers["toggle_check"] = togglecheck.HandleAccountSelection
-
 	Handlers["feedback_anonymous"] = feedback.HandleFeedbackChoice
 	Handlers["feedback_with_id"] = feedback.HandleFeedbackChoice
-
-	//Handlers["set_notifications_channel"] = setnotifications.HandleButton
-	//Handlers["set_notifications_dm"] = setnotifications.HandleButton
 	Handlers["show_interval_modal"] = setcheckinterval.HandleButton
 
 	Handlers["confirm_remove"] = removeaccount.HandleConfirmation
