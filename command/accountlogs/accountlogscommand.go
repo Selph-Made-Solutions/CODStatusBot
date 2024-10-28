4df60725628d0fa91262006726f0aa421de88192
bradselph <Bselph512@gmail.com>
2024-10-28 00:24:11 -0400

    Update accountlogscommand.go

--- command/accountlogs/accountlogscommand.go
+++ command/accountlogs/accountlogscommand.go
@@ -7,9 +7,9 @@ import (
 	"time"
 
 	"github.com/bradselph/CODStatusBot/database"
-	"github.com/bradselph/CODStatusBot/discordgo"
 	"github.com/bradselph/CODStatusBot/logger"
 	"github.com/bradselph/CODStatusBot/models"
+	"github.com/bwmarrin/discordgo"
 )
 
 func CommandAccountLogs(s *discordgo.Session, i *discordgo.InteractionCreate) {
