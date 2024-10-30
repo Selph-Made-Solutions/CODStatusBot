6e6ec4d3a0975a5f12b91564cc7a5ba5074d6251
bradselph <Bselph512@gmail.com>
2024-10-29 21:04:16 -0400

    Update models.go

--- models/models.go
+++ models/models.go
@@ -21,6 +21,7 @@ type Account struct {
 	NotificationType       string    `gorm:"default:channel"` // User preference for location of notifications either channel or dm
 	IsPermabanned          bool      `gorm:"default:false"`   // A flag indicating if the account is permanently banned
 	IsShadowbanned         bool      `gorm:"default:false"`   // A flag indicating if the account is shadowbanned
+	IsTempbanned           bool      `gorm:"default:false"`   // A flag indicating if the account is temporarily banned
 	LastCookieCheck        int64     `gorm:"default:0"`       // The timestamp of the last cookie check for permanently banned accounts.
 	LastStatusChange       int64     `gorm:"default:0"`       // The timestamp of the last status change
 	IsCheckDisabled        bool      `gorm:"default:false"`   // A flag indicating if checks are disabled for this account
