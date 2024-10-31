5e5dd2b6e6bf87cba6246993aabcfb483c35f7c1
bradselph <Bselph512@gmail.com>
2024-10-31 04:58:13 -0400

    Update sanitize.go

--- utils/sanitize.go
+++ utils/sanitize.go
@@ -6,10 +6,38 @@ import (
 )
 
 func SanitizeInput(input string) string {
-	return strings.Map(func(r rune) rune {
+	input = strings.TrimSpace(input)
+
+	input = strings.Map(func(r rune) rune {
+		if unicode.IsControl(r) {
+			return -1
+		}
+		return r
+	}, input)
+
+	input = strings.Map(func(r rune) rune {
 		if unicode.IsLetter(r) || unicode.IsNumber(r) || r == ' ' || r == '-' || r == '_' {
 			return r
 		}
 		return -1
 	}, input)
+
+	return strings.Join(strings.Fields(input), " ")
+}
+
+func SanitizeUsername(username string) string {
+	username = SanitizeInput(username)
+	if len(username) > 32 {
+		username = username[:32]
+	}
+	return username
+}
+
+func SanitizeAPIKey(apiKey string) string {
+	return strings.Map(func(r rune) rune {
+		if unicode.IsLetter(r) || unicode.IsNumber(r) {
+			return r
+		}
+		return -1
+	}, apiKey)
 }
