d990a6e2b9f710ad9215a33e9f3c010814d618f8
bradselph <Bselph512@gmail.com>
2024-11-22 06:52:22 -0500

    feat(improvement/adaptability): Update User-Agent and headers
    - Updated the User-Agent string to a more recent Chrome version
      (130.0.0.0).
    - Added `sec-fetch-dest`, `sec-fetch-site`, and `cache-control`
      headers.
    - Removed `sec-fetch-mode` header.

--- services/header.go
+++ services/header.go
@@ -5,12 +5,15 @@ import (
 )
 
 func GenerateHeaders(ssoCookie string) map[string]string {
-	return map[string]string{
-		"user-agent":     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+	headers := map[string]string{
+		"user-agent":     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
 		"accept":         "*/*",
-		"sec-fetch-mode": "cors",
+		"sec-fetch-dest": "empty",
+		"sec-fetch-site": "same-origin",
+		"cache-control":  "no-cache",
 		"cookie":         fmt.Sprintf("ACT_SSO_COOKIE=%s", ssoCookie),
 	}
+	return headers
 }
 
 func GeneratePostHeaders(ssoCookie string) map[string]string {
