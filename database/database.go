1950a1eede8fd5754ea63cae8fc8798f4c7716fa
bradselph <Bselph512@gmail.com>
2024-11-24 01:14:44 -0500

    ```go feat: Use config struct for DB credentials (improvement/adaptability)
    Refactor database connection to use a configuration struct
    for storing sensitive information like database credentials,
    instead of relying on environment variables. This change
    improves code organization, simplifies configuration
    management, and enhances security by avoiding the exposure
    of sensitive data in environment variables.
    ```

--- database/database.go
+++ database/database.go
@@ -3,12 +3,10 @@ package database
 import (
 	"errors"
 	"fmt"
-	"os"
 
-	"github.com/bradselph/CODStatusBot/models"
-
+	"github.com/bradselph/CODStatusBot/config"
 	"github.com/bradselph/CODStatusBot/logger"
-
+	"github.com/bradselph/CODStatusBot/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -17,24 +15,26 @@ var DB *gorm.DB
 
 func Databaselogin() error {
 	logger.Log.Info("Connecting to database...")
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
-	dbVar := os.Getenv("DB_VAR")
 
-	var err error
-
-	if dbUser == "" || dbPassword == "" || dbHost == "" || dbPort == "" || dbName == "" || dbVar == "" {
-		err = errors.New("one or more environment variables for database not set or missing")
-		logger.Log.WithError(err).WithField("Bot Startup ", "database variables ").Error()
+	cfg := config.Get()
+	if cfg.DBUser == "" || cfg.DBPassword == "" || cfg.DBHost == "" ||
+		cfg.DBPort == "" || cfg.DBName == "" || cfg.DBVar == "" {
+		err := errors.New("one or more database configuration values not set")
+		logger.Log.WithError(err).WithField("Bot Startup ", "database configuration ").Error()
 		return err
 	}
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s%s", dbUser, dbPassword, dbHost, dbPort, dbName, dbVar)
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s%s",
+		cfg.DBUser,
+		cfg.DBPassword,
+		cfg.DBHost,
+		cfg.DBPort,
+		cfg.DBName,
+		cfg.DBVar)
+
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		logger.Log.WithError(err).WithField("Bot Startup ", "Mysql Config ").Error()
+		logger.Log.WithError(err).WithField("Bot Startup ", "MySQL Config ").Error()
 		return err
 	}
 
