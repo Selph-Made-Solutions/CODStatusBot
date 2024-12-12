5b668f4e8d69c3566191798320e002ce5fc99708
bradselph <Bselph512@gmail.com>
2024-12-11 22:19:35 -0500

    Update header.go

--- services/header.go
+++ services/header.go
@@ -8,9 +8,12 @@ func GenerateHeaders(ssoCookie string) map[string]string {
 	headers := map[string]string{
 		"user-agent":     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
 		"accept":         "*/*",
-		"sec-fetch-dest": "empty",
-		"sec-fetch-site": "same-origin",
 		"cache-control":  "no-cache",
+		"origin":         "https://support.activision.com",
+		"referer":        "https://support.activision.com/",
+		"sec-fetch-dest": "empty",
+		"sec-fetch-mode": "cors",
+		"sec-fetch-site": "same-site",
 		"cookie":         fmt.Sprintf("ACT_SSO_COOKIE=%s", ssoCookie),
 	}
 	return headers
